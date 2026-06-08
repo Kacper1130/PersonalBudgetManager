423f0209539bed89222ee0a10daced043146aaa5
Kacper <kacperkozera13@gmail.com>
2026-06-08 11:38:45 +0200

    dockerize application

--- src/main/java/com/personalbudgetmanager/config/CorsConfiguration.java
+++ src/main/java/com/personalbudgetmanager/config/CorsConfiguration.java
@@ -14,7 +14,7 @@ class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
